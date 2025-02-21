ae03b96a9cb673f64cb537eb16761f87e6d9ee52
sunilmeena26 <sunilmeena641085@gmail.com>
2025-02-22 00:54:54 +0530

    [Sunil Meena] Add Hello Messaging Application Main Class Code File

---
+++ src/main/java/com/springapp/HelloMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMessagingApplication.class, args);
+	}
+
+}
